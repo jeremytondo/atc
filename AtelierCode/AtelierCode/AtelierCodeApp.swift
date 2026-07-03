454ce32898e9fdda65e11163593a6fdee1e3cdba
Jeremy Margaritondo <jmargari@gmail.com>
2026-07-02 22:03:07 -0500

    Initial XCode project setup including a simple hello world app.

---
+++ AtelierCode/AtelierCode/AtelierCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtelierCodeApp.swift
+//  AtelierCode
+//
+//  Created by Jeremy Margaritondo on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct AtelierCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
